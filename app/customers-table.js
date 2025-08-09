f0ff6c19bfc2161759c0fd5ea0564476b0558c13
PKennedy <kennedywpatrick@gmail.com>
2025-08-09 00:26:29 -0500

    replace table with x data grid

--- app/customers-table.js
+++ app/customers-table.js
@@ -6,11 +6,7 @@ import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import Divider from '@mui/material/Divider';
 import Stack from '@mui/material/Stack';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
+import { DataGrid } from '@mui/x-data-grid';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { mapSubscriptions } from "./utilities";
@@ -28,63 +24,85 @@ const CustomersTable = ({
   setSelectedUser,
   handleOpen
 }) => {
+
+  const columns = [
+    { field: 'actions',
+      headerName: '',
+      width: 160,
+      renderCell: (params) => (
+        <>
+          <Button disabled={open ? true : false} size="small" color="primary" onClick={e => { e.stopPropagation(); setSelectedUser(params.row); handleOpen(); }}>Update</Button>
+          <Button disabled={open ? true : false} size="small" color="error" onClick={e => { e.stopPropagation(); onDelete?.(params.row, setUsers); }}>Delete</Button>
+        </>
+      ),
+      sortable: false,
+      filterable: false,
+      disableColumnMenu: true,
+    },
+    { field: 'name',
+      headerName: 'Name',
+      width: 250,
+      renderCell: (params) => (
+        <Stack direction="row" spacing={2} sx={{ alignItems: 'center', height: '100%' }}>
+          <Avatar src={`/assets/avatar-${params.row?.avatar || "8"}.png`} />
+          <Typography variant="subtitle2">{params.row.name}</Typography>
+        </Stack>
+      ),
+      sortable: true,
+      filterable: false,
+      disableColumnMenu: true,
+    },
+    { field: 'email', headerName: 'Email', width: 250, sortable: false, filterable: false, disableColumnMenu: true, },
+    { field: 'location',
+      headerName: 'Location',
+      width: 350,
+      renderCell: (params) => (
+        <Box sx={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+          {`${params.row.street || ''} ${params.row.city || ''} ${params.row.state || ''}, ${params.row.country || ''}`}
+        </Box>
+      ),
+      sortable: false,
+      filterable: false,
+      disableColumnMenu: true,
+    },
+    { field: 'phone', headerName: 'Phone', width: 140, sortable: false, filterable: false, disableColumnMenu: true, },
+    { field: 'subscriptions',
+      headerName: 'Subscriptions',
+      width: 160,
+      renderCell: (params) => (
+        <Box sx={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+          {mapSubscriptions(params.row.subscriptions)}
+        </Box>
+      ),
+      sortable: false,
+      filterable: false,
+      disableColumnMenu: true,
+    },
+    { field: 'purchaseHistory', headerName: 'Purchase History', width: 180, sortable: false, filterable: false, disableColumnMenu: true, },
+  ];
+
   return (
     <>
-    <Card>
-      <Box sx={{ overflowX: 'auto' }}>
-        <Table sx={{ minWidth: '800px' }}>
-          <TableHead>
-            <TableRow>
-              <TableCell padding="checkbox">
-              </TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>Email</TableCell>
-              <TableCell>Location</TableCell>
-              <TableCell>Phone</TableCell>
-              <TableCell>Subscriptions</TableCell>
-              <TableCell>Purchase History</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => {
-              return (
-                <TableRow hover key={row?.id} onClick={() => {
-                  setSelectedUser(row)
-                  handleOpen()
-                }}>
-                  <TableCell>
-                    <Button disabled={open ? true : false} size="small" color={"primary"} onClick={() => {
-                      setSelectedUser(row)
-                      handleOpen()
-                    }}>
-                      Update
-                    </Button>
-                    <Button disabled={open ? true : false} size="small" color={"error"} onClick={() => onDelete?.(row, setUsers)}>
-                      Delete
-                    </Button>
-                  </TableCell>
-                  <TableCell>
-                    <Stack sx={{ alignItems: 'center' }} direction="row" spacing={2}>
-                      <Avatar src={`/assets/avatar-${row?.avatar || "8"}.png`} />
-                      <Typography variant="subtitle2">{row.name}</Typography>
-                    </Stack>
-                  </TableCell>
-                  <TableCell>{row?.email}</TableCell>
-                  <TableCell>
-                    {row?.street} {row?.city} {row?.state}, {row?.country}
-                  </TableCell>
-                  <TableCell>{row?.phone}</TableCell>
-                  <TableCell>{mapSubscriptions(row?.subscriptions)}</TableCell>
-                  <TableCell>{row?.purchaseHistory}</TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </Box>
-      <Divider />
-    </Card>
-    <UserPopover onClose={onClose} open={open} signOut={signOut} selectedUser={selectedUser} onUpdate={onUpdate}  />
+
+        <Box sx={{ height: '100%', width: '1500px' }}>
+          <DataGrid
+            showToolbar
+            disableColumnFilter
+            rows={rows}
+            columns={columns}
+            pageSize={10}
+            rowsPerPageOptions={[10, 25, 50]}
+            disableSelectionOnClick
+            onRowClick={(params) => {
+              setSelectedUser(params.row);
+              handleOpen();
+            }}
+            getRowId={row => row.id}
+          />
+        </Box>
+        <Divider />
+
+      <UserPopover onClose={onClose} open={open} signOut={signOut} selectedUser={selectedUser} onUpdate={onUpdate}  />
     </>
   );
 }
