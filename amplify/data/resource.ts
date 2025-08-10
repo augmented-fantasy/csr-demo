199e1dbc3c849f2ba8f92f7497093b3a9dea6d1b
PKennedy <kennedywpatrick@gmail.com>
2025-08-09 22:28:01 -0500

    Set key expiration

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -30,7 +30,7 @@ export const data = defineData({
   authorizationModes: {
     defaultAuthorizationMode: "apiKey",
     apiKeyAuthorizationMode: {
-      expiresInDays: 1,
+      expiresInDays: 3,
     },
   },
 });
